120c4d36a4e69a692246ad6eaa5d0582b1a2b32a
Maxine <maxine.hanrieder@online.de>
2019-05-11 18:02:59 +0200

    first dws

--- src/methods/dws.py
+++ src/methods/dws.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import pywt
 import pywt.data
+from sklearn.preprocessing import MinMaxScaler
 
 from src.methods.method import Method
 
@@ -22,7 +23,21 @@ class DWS(Method):
         self.save_name = save_name
 
     def detect_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
-        raise NotImplementedError
+        # 1. Normalize and decompose time series
+        x_normalized = self.normalize(time_series_x)
+
+        # 2. Calculate DWS
+
+        # 3. Generate noise & calculate RDWS
+        mu, sigma = 0, 1  # mean and standard deviation
+        noise = np.random.normal(mu, sigma, len(time_series_y))
+        noise = self.normalize(noise)
+
+        # 4. Repeat 3. 5000 times, calculate mean & variance of all RDWSs. Confidence interval = 95%
 
     def estimate_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
         raise NotImplementedError
+
+    def normalize(self, x: np.array):
+        scaler = MinMaxScaler((-1, 1))
+        return scaler.fit_transform(x)
