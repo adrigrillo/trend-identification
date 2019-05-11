18ab428e9925abd2fef7557d0ccafa36b0a27b4c
Maxine <maxine.hanrieder@online.de>
2019-05-11 13:05:41 +0200

    update EMD

--- src/methods/emd.py
+++ src/methods/emd.py
@@ -1,5 +1,6 @@
 from PyEMD import EMD, Visualisation
 from src.methods.method import Method
+import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -9,21 +10,22 @@ class EmpiricalModeDecomposition(Method):
         emd = EMD()
         emd.emd(time_series_x)
         imfs, res = emd.get_imfs_and_residue()
-        return imfs[:-1]
+        return imfs[-1]
 
     def detect_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
+        raise NotImplementedError('This method does not have the capability of detecting a trend')
+
+    def visualize_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
         trend = self.estimate_trend(time_series_x, time_series_y)
-        threshold = 0.05
-        if np.abs(trend[:-1] - trend[0]) > threshold:
-            return True
-        else:
-            return False
+
+        plt.figure(figsize=(6, 2))
+        plt.plot(trend)
+        plt.xlabel('Time')
+        plt.ylabel('Trend')
+        plt.show()
 
 
-
-    #def visualize_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
-
-
+# test time series:
 t = np.arange(0, 3, 0.01)
 S = np.sin(13*t + 0.2*t**1.4) - np.cos(3*t)
 S1 = t
@@ -31,13 +33,10 @@ S2 = np.cos(22 / np.pi * t**2) + 6 * t**2
 S3 = np.empty(len(t))
 S3.fill(0.5)
 
-emd_test = EMD()
-emd_test.emd(S)
-imfs_test, res_test = emd_test.get_imfs_and_residue()
+emd = EmpiricalModeDecomposition()
+emd.visualize_trend(S1, t)
 
-print(imfs_test[0])
-print(imfs_test[1])
 
-vis = Visualisation()
-vis.plot_imfs(imfs=imfs_test, residue=res_test, t=t, include_residue=True)
-vis.show()
+
+
+
