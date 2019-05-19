5bf648b6a1838402cfc695b623c0c1697e7f866e
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-05-19 12:04:59 +0200

    Theil can estimate the trend now #6

--- src/methods/emd.py
+++ src/methods/emd.py
@@ -6,16 +6,16 @@ from src.methods.method import Method
 
 
 class EmpiricalModeDecomposition(Method):
+    def detect_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
+        trend = self.estimate_trend(time_series_x, time_series_y)
+        return self.describe_trend_from_array(time_series_x, trend)
+
     def estimate_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
         # Extract imfs and residue
         emd = EMD()
         emd.emd(time_series_y)
         imfs, res = emd.get_imfs_and_residue()
         return imfs[-1]
-
-    def detect_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
-        trend = self.estimate_trend(time_series_x, time_series_y)
-        return self.describe_trend_from_array(time_series_x, trend)
 
     def visualize_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
         trend = self.estimate_trend(time_series_x, time_series_y)
