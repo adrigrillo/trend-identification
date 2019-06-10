805ab48af147ddc4ae873f5ba91714c3c34135ff
Adrián Rodríguez Grillo <adrirgrillo@gmail.com>
2019-06-10 13:15:49 +0200

    Added autoarima library and implemented in estimate trend, needs revision #12

--- src/methods/arima.py
+++ src/methods/arima.py
@@ -1,32 +1,54 @@
 from sys import platform as sys_pf
+
 if sys_pf == 'darwin':
     import matplotlib
+
     matplotlib.use("TkAgg")
     import matplotlib.pyplot as plt
 
 import numpy as np
 import sys
-import pandas as pd
-from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from scipy.optimize import brute
 from src.methods.method import Method
 import numpy.random as random
 from sklearn.model_selection import ParameterGrid
+import pmdarima as pm
+import matplotlib.pyplot as plt
 
-x = np.linspace(0, 100, num=100)
-trend = x + 25 * np.sin(x/6)
-y = x + 25 * np.sin(x/6) + 12 * random.randn(len(x))
-# y = np.sin(y)
-
-# plt.plot(y)
-# plt.plot(trend)
-# plt.show()
 
 class Arima(Method):
 
+    def __init__(self, seasonality: int = 1,
+                 trace: bool = False, suppress_warnings: bool = True,
+                 error_action: str = 'warn'):
+        """
+        Auto ARIMA initializer
+
+        :param seasonality: The period for seasonal differencing, m refers to the number of periods
+        in each season. For example, m is 4 for quarterly data, 12 for monthly data, or 1 for annual
+        (non-seasonal) data. Default is 1. Note that if m == 1 (i.e., is non-seasonal),
+        seasonal will be set to False.
+        :param trace: whether to print status on the fits. Note that this can be very verbose.
+        :param suppress_warnings: many warnings might be thrown inside of statsmodels.
+        If suppress_warnings is True, all of the warnings coming from ARIMA will be squelched.
+        :param error_action: If unable to fit an ARIMA due to stationarity issues, whether to warn (‘warn’),
+        raise the ValueError (‘raise’) or ignore (‘ignore’). Note that the default behavior is to warn,
+        and fits that fail will be returned as None. This is the recommended behavior, as statsmodels
+        ARIMA and SARIMAX models hit bugs periodically that can cause an otherwise healthy parameter
+        combination to fail for reasons not related to pmdarima.
+        """
+        self.seasonality = seasonality
+        self.trace = trace
+        self.suppress_warnings = suppress_warnings
+        self.error_action = error_action
+
     def estimate_trend(self, time_series_x: np.ndarray, time_series_y: np.ndarray):
-        pass
+        arima_fit = pm.auto_arima(time_series_y,
+                                  m=self.seasonality,
+                                  trace=self.trace,
+                                  error_action=self.error_action,
+                                  suppress_warnings=self.suppress_warnings)
+        return arima_fit
 
     def detect_trend(self, time_series: np.ndarray, y):
         # series = np.concatenate((x.reshape(len(x),1),y.reshape(len(y),1)),axis=1)
@@ -40,20 +62,20 @@ class Arima(Method):
         pq_bound = 5
         PQ_bound = 2
 
-        param_grid = list(ParameterGrid({'m':range(2,30,3), 'd':range(3), 'D':range(3)}))
+        param_grid = list(ParameterGrid({'m': range(2, 30, 3), 'd': range(3), 'D': range(3)}))
 
         # Create function for searching the variation
         def objfunc(order):
             try:
                 order, sorder = order[:3], order[3:]
-                fit = SARIMAX(y,x, order, sorder).fit(disp=False)
+                fit = SARIMAX(y, x, order, sorder).fit(disp=False)
                 # Return AIC, ignoring nans as invalid models are not good fits
                 return fit.aic if (not np.isnan(fit.aic)) else sys.float_info.max
             except Exception as e:
                 # Ignore exceptions, they are likely poor fits.
                 return sys.float_info.max
 
-        grid_order = (0,0,0,0,0,0,0)
+        grid_order = (0, 0, 0, 0, 0, 0, 0)
         grid_aic = sys.float_info.max
 
         for params in param_grid:
@@ -62,14 +84,14 @@ class Arima(Method):
             D = params['D']
             m = params['m']
 
-            print(d,D,m)
+            print(d, D, m)
 
             # Step 1
             initial_models = [
-                (2,d,2,1,D,1,m),
-                (0,d,0,0,D,0,m),
-                (1,d,0,1,D,0,m),
-                (0,d,1,0,D,1,m)
+                (2, d, 2, 1, D, 1, m),
+                (0, d, 0, 0, D, 0, m),
+                (1, d, 0, 1, D, 0, m),
+                (0, d, 1, 0, D, 1, m)
             ]
 
             aics = list(map(lambda x: objfunc(x), initial_models))
@@ -84,23 +106,25 @@ class Arima(Method):
                 # Create the order sets
                 orders = [
                     # p,q,P,D can each vary by +-1
-                    (p-1,d,q,P,D,Q,m),
-                    (p,d,q-1,P,D,Q,m),
-                    (p,d,q,P-1,D,Q,m),
-                    (p,d,q,P,D,Q-1,m),
-                    (p+1,d,q,P,D,Q,m),
-                    (p,d,q+1,P,D,Q,m),
-                    (p,d,q,P+1,D,Q,m),
-                    (p,d,q,P,D,Q+1,m),
+                    (p - 1, d, q, P, D, Q, m),
+                    (p, d, q - 1, P, D, Q, m),
+                    (p, d, q, P - 1, D, Q, m),
+                    (p, d, q, P, D, Q - 1, m),
+                    (p + 1, d, q, P, D, Q, m),
+                    (p, d, q + 1, P, D, Q, m),
+                    (p, d, q, P + 1, D, Q, m),
+                    (p, d, q, P, D, Q + 1, m),
                     # p,q or P,Q may vary in combination by +-1
-                    (p-1,d,q-1,P,D,Q,m),
-                    (p+1,d,q+1,P,D,Q,m),
-                    (p,d,q,P-1,D,Q-1,m),
-                    (p,d,q,P+1,D,Q+1,m),
+                    (p - 1, d, q - 1, P, D, Q, m),
+                    (p + 1, d, q + 1, P, D, Q, m),
+                    (p, d, q, P - 1, D, Q - 1, m),
+                    (p, d, q, P + 1, D, Q + 1, m),
                 ]
 
                 # Prune options based on constraints
-                orders = list(filter(lambda x: x[0] <= pq_bound and x[2] <= pq_bound and x[3] <= PQ_bound and x[5] <= PQ_bound, orders))
+                orders = list(
+                    filter(lambda x: x[0] <= pq_bound and x[2] <= pq_bound and x[3] <= PQ_bound and x[5] <= PQ_bound,
+                           orders))
                 # Negative orders are not sensible
                 orders = list(filter(lambda x: x[0] >= 0 and x[2] >= 0 and x[3] >= 0 and x[5] >= 0, orders))
                 # Roots of psis & thetas constraint?
@@ -125,7 +149,6 @@ class Arima(Method):
                 grid_order = current_order
             print('Current best: ', grid_order, ' aic: ', grid_aic)
 
-
         #
         #   </auto.arima>
         #
@@ -137,21 +160,20 @@ class Arima(Method):
         model = SARIMAX(y, x, grid_order[:3], grid_order[3:])
         model_fit = model.fit(disp=False)
 
-
         return model_fit, grid_order, model
 
 
-m_fit, opt_order, model = Arima().detect_trend(x, y)
+if __name__ == '__main__':
+    x = np.linspace(0, 100, num=100)
+    trend = x + 25 * np.sin(x / 6)
+    y = x + 25 * np.sin(x / 6) + 12 * random.randn(len(x))
 
-print(m_fit.summary())
-print('Parameters: ', m_fit.params)
-pred = m_fit.predict(start=45, end=99)
+    arima = Arima(seasonality=7)
+    fit = arima.estimate_trend(x, y)
 
-print(np.sum(np.abs((y[45:] - pred) - trend[45:])))
+    plt.plot()
 
-plt.plot()
-
-plt.plot(y)
-plt.plot(pred)
-plt.plot(trend)
-plt.show()
+    plt.plot(y, label='Real')
+    plt.plot(fit.predict(y.shape[0]), label='prediction')
+    plt.legend()
+    plt.show()
