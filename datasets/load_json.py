6b4a3167cf39977291c447398306562f74cf7a14
Jessica Wiezorek <jesswiezorek@gmail.com>
2019-06-12 14:04:30 +0200

    nichts

--- datasets/load_json.py
+++
@@ -1,6 +0,0 @@
-import json
-import numpy as np
-with open('C:\\Users\\Jessica\\Documents\\Master\\PDSDMY2\\datasets\\out\\gold_monthly_json_out.txt') as json_file:  
-    data = json.load(json_file)
-trend = np.array(data['data'])
-print(trend)
