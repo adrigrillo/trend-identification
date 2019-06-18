ca8d6884da32f18a2d28442b084abf8127834c95
Maxine Hanrieder <maxine.hanrieder@online.de>
2019-06-18 14:19:21 +0200

    bug fix

--- src/data_handler/data_generator.py
+++ src/data_handler/data_generator.py
@@ -35,14 +35,14 @@ def generate_data(name: str = 'time_series_',
         'a*x**(1/2)',
         'a*x**(1/3)',
         'a*x**3 - a*x**2',
-        'a*x**5 - a*x**2 + a*x'
+        'a*x**5 - a*x**2 + a*x',
         'a*np.sin((x*2*np.pi)/(b*2+1) + c*np.pi)',
         'a*5**x'
     ]
 
-    trends: list = np.empty([0,len(trend_structures)])
+    trends: list = np.empty([0, len(trend_structures)])
 
-    coefficients = np.empty([0,3])
+    coefficients = np.empty([0, 3])
 
     # This is done to remember what random values were set for the coefficients.
     for i in range(10):
@@ -58,33 +58,6 @@ def generate_data(name: str = 'time_series_',
 
         coefficients = np.append(coefficients, [[a, b, c]], axis=0)
 
-    # polynomials = [''] * (poly_max_degree * poly_slope_steps)
-    # sinusoidals = ['np.sin(x*2*np.pi)', 'np.sin(x*2*np.pi)',
-    #                '10*np.sin(x*2*np.pi)', '10*np.sin(x*2*np.pi)',
-    #                '100*np.sin(x*2*np.pi)', '100*np.sin(x*2*np.pi)']
-    #
-    # for i in range(len(polynomials)):
-    #     for degree in range(1, poly_max_degree + 1):
-    #         for slope in range(1, poly_slope_steps + 1):
-    #             if slope:
-    #                 polynomials[i] = str(round(random.uniform(-1, 1), 2)) + '*x**' + str(degree)
-    #                 # sinusoidals[i] = str(round(random.uniform(-10, 10), 2)) + \
-    #                 #                 '*np.sin(' + \
-    #                 #                 str(round(random.uniform(0.6, 1), 2)*2*np.pi/300) + \
-    #                 #                 '*x)'
-    #
-    # trends = polynomials + sinusoidals
-    #
-    # print(np.array(trends).dtype)
-    #
-    # number_generated_time_series = len(trends) * len(sig_noise_ratio) * len(seasonalities)
-    # generated_xs = \
-    #     generated_ys = \
-    #     generated_trends = \
-    #     generated_seasonalities = \
-    #     generated_noises = \
-    #     [[0] for y in range(number_generated_time_series)]
-
     index = 0
     for i in range(len(trends)):
         for noise in sig_noise_ratio:
@@ -98,13 +71,6 @@ def generate_data(name: str = 'time_series_',
                     create_function_file(name + str(index), trend, data_points, noise, seasonality, coefficients[i], trend_structures[j])
                     # create .csv data file
                     generate_synthetic_data('function', name + str(index) + '.ini')
-                    # generated_xs[index-1], \
-                    #     generated_ys[index-1], \
-                    #     generated_trends[index-1], \
-                    #     generated_seasonalities[index-1], \
-                    #     generated_noises[index-1] = \
-
-    # return generated_xs, generated_ys, generated_trends, generated_seasonalities, generated_noises
 
 
 if __name__ == '__main__':
